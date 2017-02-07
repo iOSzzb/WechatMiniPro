2333e6d4a0a30ba731f549b635bc3b817518129f
张振波 <zhenbo.zhang@sino-life.com>
2017-02-07 17:01:44 +0800

    文档看到了组件

--- pages/newpage/newpage.js
+++ pages/newpage/newpage.js
@@ -1,10 +1,15 @@
 //newpage.js
 Page({
     data: {
-        text: "this is data",
-        zero: "0",
-        a:2,
-        b:3
+        objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4]
     },
     onLoad: function(){
         console.log("new page on load");
@@ -14,5 +19,34 @@ Page({
         this.setData({
             text: "chang data"
         })
+    },
+    switch: function(e){
+        console.log("tap switch button");
+        const length = this.data.objectArray.length;
+        for (let i = 0; i < length; ++i){
+            const x = Math.floor(Math.random()*length);
+            const y = Math.floor(Math.random()*length);
+            const temp = this.data.objectArray[x];
+            this.data.objectArray[x] = this.data.objectArray[y];
+            this.data.objectArray[y] = temp;
+        }
+        this.setData({
+            objectArray:this.data.objectArray
+        });
+    },
+    addToFront: function(e){
+        const length = this.data.objectArray.length;
+        this.data.objectArray = [{id:length,unique:'unique_'+length}].concat(this.data.objectArray);
+        this.setData({
+            objectArray:this.data.objectArray
+        });
+    },
+    addNumberToFront: function(e){
+        this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray);
+        this.setData(
+            {
+                numberArray:this.data.numberArray
+            }
+        );
     }
 })
